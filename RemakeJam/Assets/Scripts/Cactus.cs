872bab7ad4285db28d562b406801b8980aad6e94
Pattnatt <jittapat007@gmail.com>
2017-06-19 14:51:21 +0700

    Create Cactus Prefab

---
+++ RemakeJam/Assets/Scripts/Cactus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour {
+
+    public static int STATUS_NORMAL = 0;
+    public static int STATUS_INVINCIBLE = -1;
+    public static int STATUS_DEAD = -2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
