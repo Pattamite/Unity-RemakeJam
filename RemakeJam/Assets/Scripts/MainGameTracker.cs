0d895b180d831941baf0c864297ee42958b6d5ed
Quaruzuz <Quaruzuz@gmail.com>
2017-06-20 19:52:29 +0700

    [Add] : CactusSpawner (Falls from sky) [Fix] : Add floor level (water level)

--- RemakeJam/Assets/Scripts/MainGameTracker.cs
+++ RemakeJam/Assets/Scripts/MainGameTracker.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class MainGameTracker : MonoBehaviour
 {
     public static float GAME_SPEED = 1.0f;
+    public static float FLOOR_LEVEL = -4.0f;
     [Range(0.0f, 10.0f)] public float gameSpeed = 1.0f;
+    [Range(-4.0f, 8.0f)] public float floorLevel = -4.0f;
 
-	void Start ()
+    void Start ()
     {
-		
-	}
-	
-	void Update ()
+    }
+
+    void Update ()
     {
         GAME_SPEED = gameSpeed;
+        FLOOR_LEVEL = floorLevel;
     }
 }
