38216545f899036d9111f846a340e566751b4e96
Pattnatt <jittapat007@gmail.com>
2017-06-21 15:49:21 +0700

    Add Mainmenu

---
+++ RemakeJam/Assets/Scripts/MainManuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManuManager : LevelManager
+{
+
+	// Use this for initialization
+	void Start () {
+		if(!PlayerPrefs.HasKey(PlayerPrefsKeys.HIGH_SCORE))
+        {
+            PlayerPrefs.SetInt(PlayerPrefsKeys.HIGH_SCORE, -1);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
