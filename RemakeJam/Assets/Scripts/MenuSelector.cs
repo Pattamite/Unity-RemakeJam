56095664b87fcca1e0c02266bb98fbd226cd4723
Pattnatt <jittapat007@gmail.com>
2017-06-25 17:33:16 +0700

    Menu can access by keyboard

---
+++ RemakeJam/Assets/Scripts/MenuSelector.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuSelector : MonoBehaviour {
+
+    public Button[] button;
+    private int currentSelect;
+    private bool isMouseUsed;
+
+    void Start()
+    {
+        currentSelect = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMouse();
+
+        if (currentSelect != -1 && isMouseUsed)
+        {
+            currentSelect = -1;
+            ClearSelect();
+        }
+        else KeyboardSelect();
+    }
+
+    private void CheckMouse()
+    {
+        isMouseUsed = false;
+        for (int i = 0 ; i < button.Length ; i++)
+        {
+            print(button[i] + " / " + button[i].gameObject.GetComponent<MouseDetector>().isMouseSelected);
+            if (button[i].gameObject.GetComponent<MouseDetector>().isMouseSelected)
+            {
+                isMouseUsed = true;
+                //break;
+            }
+        }
+    }
+
+    private void ClearSelect()
+    {
+        EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(null);
+    }
+
+    private void KeyboardSelect()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            if (currentSelect != -1)
+            {
+                ClearSelect();
+                button[currentSelect].onClick.Invoke();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentSelect == -1)
+            {
+                currentSelect = 0;
+            }
+            else
+            {
+                ClearSelect();
+                currentSelect = (currentSelect + 1) % button.Length;
+            }
+            button[currentSelect].Select();
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (currentSelect == -1)
+            {
+                currentSelect = button.Length - 1;
+            }
+            else
+            {
+                ClearSelect();
+                currentSelect = (currentSelect - 1 + button.Length) % button.Length;
+            }
+            button[currentSelect].Select();
+        }
+    }
+}
---
+++ RemakeJam/Assets/Scripts/MouseDetector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public bool isMouseSelected;
+	void Start () {
+        isMouseSelected = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isMouseSelected = true;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isMouseSelected = false;
+    }
+}
